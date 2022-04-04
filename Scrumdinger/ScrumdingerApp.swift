04f8434442470ed2edc05457303653323a876382
FARABI DHARMA <74946238+farabidharma88@users.noreply.github.com>
2022-04-04 14:48:54 +0700

    Add files via upload

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView{
+                ScrumsView(scrums: DailyScrum.sampleData)
+            }
+        }
+    }
+}
